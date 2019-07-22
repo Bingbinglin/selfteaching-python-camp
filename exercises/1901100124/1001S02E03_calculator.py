ac6bf0e5e5eaed784587c7400f3635bd4007d41f
Bingbinglin <44563085+Bingbinglin@users.noreply.github.com>
2019-07-22 08:15:56 +0800

    1901100124 day3 自学训练营学习16群

---
+++ exercises/1901100124/1001S02E03_calculator.py
@@ -0,0 +1,17 @@
+operator=input("选择运算（+、-、*、/）:")
+number1=int(input("请输入第一个数字："))
+number2=int(input("请输入第二个数字："))
+a=number1
+b=number2
+if operator == "+":
+    print(a,"+",b,"=",a+b)
+elif operator == "-":
+    print(a,"-","=",a-b)
+elif operator == "*":
+    print(a,"*",b,"=",a*b)
+elif operator == "/":
+    print(a,"/",b,"=",a*b)
+else :
+    print("无效运算")
+    
+    
